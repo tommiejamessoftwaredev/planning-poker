e26108728c40c4ab94f76457f6f1e32d3c793014
tommiejames95 <tommiejames.softwaredev@gmail.com>
2024-05-26 04:01:58 +0100

    update cors 2

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 });
 
 app.use(cors({
-  origin: "http://localhost:3000"
+  origin: ["http://localhost:3000", "https://planning-poker-100.azurewebsites.net"]
 }));
 
 app.use(express.static(path.join(__dirname, '../planning-poker/build')));
